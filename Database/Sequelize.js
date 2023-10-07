d7d55781318fb40cdc5e854ad92a7a3cb5fc4ae1
Marianna Lopes Américo <Marianna.l.americo@gmail.com>
2023-10-07 18:17:10 -0300

    Preparando ambiente

---
+++ Database/Sequelize.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
+    host: process.env.PG_HOST,
+    dialect: 'postgres'
+  });
+
+async function conectar(){
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+conectar();
+
+module.exports = sequelize;
